f98eb679675355ead79f641399897cefe0db6047
Kahdes <maxime.dosse@gmail.com>
2018-06-07 13:27:54 +0200

    Modification de structure/style du footer

--- View/footer.php
+++ View/footer.php
@@ -1,7 +1,10 @@
-<footer>
-    <div class="col-xs-12" id="footer-content">
-    	<div class="col-xs-12 col-md-10 col-md-offset-1">
-    		<h1>FOOTER</h1>
-    	</div>        
+<footer class="container-fluid">
+    <div class="col-xs-12" id="footer-info">
+    	<p>
+    		<button class="btn btn-link"><a href="index.php?action=connexion">Connexion</a></button>
+    		/
+    		<button class="btn btn-link"><a href="index.php?action=connexion">Gestion</a></button>
+    	</p>
+    	<p>Copyright © maximedosse.com 2018</p>       
     </div>
 </footer>
