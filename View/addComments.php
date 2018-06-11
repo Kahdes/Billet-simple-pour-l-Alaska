9daa0cb6d0d44719ff2637cac0763c592c8c538a
Kahdes <maxime.dosse@gmail.com>
2018-06-11 13:33:25 +0200

    Modifications structure de formulaire

--- View/addComments.php
+++ View/addComments.php
@@ -1,9 +1,20 @@
 <section class="row" id="bill-new-comment">
 	<div class="col-xs-12 col-md-10 col-md-offset-1">
-		<form>
+		<h2>Ajouter un nouveau commentaire :</h3>
+			
+		<br/>
+
+		<form action="" method="post">
 			<div class="form-group">
-				<h2>Ajouter un nouveau commentaire :</h2>
+				<textarea name="message" id="message"></textarea>
 			</div>
+				
+			<div class="form-group">
+				<label for="pseudo">Pseudo : </label><br/>			
+				<input class="form-control" type="text" name="pseudo" id="pseudo" required />
+			</div>
+
+			<input class="btn btn-info form-control" type="submit" name="" id="" />
 		</form>
 	</div>
 </section>
