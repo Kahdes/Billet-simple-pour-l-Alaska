f3748ce0e4291abde5b7adf10048f21d98cdbfc0
Kahdes <maxime.dosse@gmail.com>
2018-06-11 10:59:02 +0200

    Ajout comments.php & addComments.php

---
+++ View/addComments.php
@@ -0,0 +1,9 @@
+<section class="row" id="bill-new-comment">
+	<div class="col-xs-12 col-md-10 col-md-offset-1">
+		<form>
+			<div class="form-group">
+				<h2>Ajouter un nouveau commentaire :</h2>
+			</div>
+		</form>
+	</div>
+</section>
---
+++ View/comments.php
@@ -0,0 +1,31 @@
+<section class="row" id="comment-list">
+	<div class="col-xs-12 col-md-10 col-md-offset-1">
+		<h2>Derniers commentaires :</h1>
+		<br/>
+
+		<?php
+			foreach($comments as $c) {
+		?>
+			<article class="panel">
+				<h3 class="panel-heading" id="comment-title">
+					<?= htmlspecialchars($c['pseudo']);?>					
+				</h1>
+
+				<p id="comment-date">
+					<em>
+						Publié le : <?= htmlspecialchars($c['dateFR']);?>
+					</em>
+				</p>
+
+				<hr/>
+				
+				<p class="panel-body" id="comment-body">
+					<?= htmlspecialchars($c['contenu']);?> 
+					<br/><br/>
+				</p>
+			</article>
+		<?php
+			}
+		?>
+	</div>
+</section>
