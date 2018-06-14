38e0ca3fdd55298cfe593ab3b006df2459e8d9cd
Kahdes <maxime.dosse@gmail.com>
2018-06-14 23:37:54 +0200

    Ajout icônes navbar / Suppression Bootstrap local & Ajout Bootstrap CDN

--- View/navbar.php
+++ View/navbar.php
@@ -4,8 +4,12 @@
             <div class="navbar-header">
                 <a class="navbar-brand" href="index.php">Billet simple pour l'Alaska</a>
             </div>  
-            <li class="navbar-link" id="accueil"><a href="index.php">Accueil</a></li>
-            <li class="navbar-link" id="billList"><a href="index.php?action=billList" id="billList">Liste des billets</a></li>      
+            <li class="navbar-link" id="accueil">
+            	<a href="index.php"><span class="glyphicon glyphicon-home"></span> Accueil</a>
+            </li>
+            <li class="navbar-link" id="billList">
+            	<a href="index.php?action=billList"><span class="glyphicon glyphicon-book"></span> Liste des billets</a>
+            </li>      
         </ul>
     </div>
 </nav>
--- View/template.php
+++ View/template.php
@@ -10,9 +10,14 @@
     <meta name="description" content="<?= $description; ?>">
 
     <link rel="icon" type="image/png" href="Public/images/icon.jpg" />
-        
+    
+    <!--    
     <link rel="stylesheet" type="text/css" href="Public/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="Public/css/bootstrap-theme.min.css" />
+    -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    
     <link rel="stylesheet" type="text/css" href="Public/css/style.css" />
     <link rel="stylesheet" type="text/css" href="Public/css/queries.css" />
 
@@ -51,7 +56,7 @@
         }    
     ?>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </body>
 </html>
