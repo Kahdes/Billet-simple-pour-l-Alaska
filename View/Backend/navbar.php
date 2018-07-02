a65dac6370503ef4416c599785124ae3072353f6
Kahdes <maxime.dosse@gmail.com>
2018-07-02 11:16:47 +0200

    Ajout menu hamburger Navbar / Réorganisation du code

--- View/Backend/navbar.php
+++ View/Backend/navbar.php
@@ -1,23 +1,32 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container-fluid">
-        <ul class="nav navbar-nav">
         <div class="navbar-header">
             <a class="navbar-brand" href="index.php">Billet simple pour l'Alaska</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
         </div>
-            <li class="navbar-link" id="dashboard">
-                <a href="index.php?action=dashboard"><span class="glyphicon glyphicon-inbox"></span> Tableau de bord</a>
-            </li>         
-            <li class="navbar-link" id="create">
-       	        <a href="index.php?action=dashboard&admin=create"><span class="glyphicon glyphicon-book"></span> Créer un billet</a>
-            </li>
-            <li class="navbar-link" id="dashboard-home">
-                <a href="index.php"><span class="glyphicon glyphicon-home"></span> Site</a>
-            </li>
-        </ul>
-        <form class="navbar-form navbar-right inline-form" action="index.php" method="post">
-            <div class="form-group">
-                <button class="btn btn-danger btn-sm" type="submit" value="disconnect" name="disconnect">Déconnexion</button>
-            </div>
-        </form>
+
+        <div class="collapse navbar-collapse" id="navbar-collapse-2">
+            <ul class="nav navbar-nav">
+                <li class="navbar-link" id="dashboard">
+                    <a href="index.php?action=dashboard"><span class="glyphicon glyphicon-inbox"></span> Tableau de bord</a>
+                </li>         
+                <li class="navbar-link" id="create">
+                    <a href="index.php?action=dashboard&admin=create"><span class="glyphicon glyphicon-book"></span> Créer un billet</a>
+                </li>
+                <li class="navbar-link" id="dashboard-home">
+                    <a href="index.php"><span class="glyphicon glyphicon-home"></span> Site</a>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right inline-form" action="index.php" method="post">
+                <div class="form-group">
+                    <button class="btn btn-danger btn-sm" type="submit" value="disconnect" name="disconnect">Déconnexion</button>
+                </div>
+            </form>
+        </div>
     </div>
 </nav>
--- View/Frontend/navbar.php
+++ View/Frontend/navbar.php
@@ -1,35 +1,44 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container-fluid">
-        <ul class="nav navbar-nav">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">Billet simple pour l'Alaska</a>
-            </div>  
-            <li class="navbar-link" id="accueil">
-            	<a href="index.php"><span class="glyphicon glyphicon-home"></span> Accueil</a>
-            </li>
-            <li class="navbar-link" id="billList">
-            	<a href="index.php?action=billList"><span class="glyphicon glyphicon-book"></span> Liste des billets</a>
-            </li>
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">Billet simple pour l'Alaska</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="navbar-link" id="accueil">
+                    <a href="index.php"><span class="glyphicon glyphicon-home"></span> Accueil</a>
+                </li>
+                <li class="navbar-link" id="billList">
+                    <a href="index.php?action=billList"><span class="glyphicon glyphicon-book"></span> Liste des billets</a>
+                </li>
 <?php
     if (isset($_SESSION['account']) && isset($_SESSION['password'])) {
 ?>
-        <li class="navbar-link" id="gestion">
-            <a href="index.php?action=dashboard"><span class="glyphicon glyphicon-bookmark"></span> Gestion</a>
-        </li>
+                <li class="navbar-link" id="gestion">
+                    <a href="index.php?action=dashboard"><span class="glyphicon glyphicon-bookmark"></span> Gestion</a>
+                </li>
 <?php
     }
 ?>
-        </ul>
+            </ul>
 <?php
     if (isset($_SESSION['account']) && isset($_SESSION['password'])) {
 ?>
-        <form class="navbar-form navbar-right inline-form" action="index.php" method="post">
-            <div class="form-group">
-                <button class="btn btn-danger btn-sm" type="submit" value="disconnect" name="disconnect">Déconnexion</button>
-            </div>
-        </form>
+            <form class="navbar-form navbar-right inline-form" action="index.php" method="post">
+                <div class="form-group">
+                    <button class="btn btn-danger btn-sm" type="submit" value="disconnect" name="disconnect">Déconnexion</button>
+                </div>
+            </form>
 <?php
     }
 ?>
+        </div>
     </div>
 </nav>
